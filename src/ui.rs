91ea6725b16d2b2e37a1644dd23c31303381c86f
cmbb <zhangyining098765432@126.com>
2022-11-18 23:33:24 -0500

    UI Module Version 0 (temporary)

--- src/ui.rs
+++ src/ui.rs
@@ -1,14 +1,15 @@
 use std::io;
+use std::path::Path;
 
 pub fn receive_input_command(input_test: &str) ->io::Result<()>{
     Ok(())
 
 }
 
-fn match_command(wd:path,input:Vec<&str>)->Vec<&str>{
+fn match_command<'a>(wd:&'a str,input:Vec<&'a str>)->Vec<&'a str>{
     input
 }
 
-fn input_handling(return_result:Result<(), E>){
-    println!(return_result)
+fn input_handling<E: std::fmt::Debug>(return_result:Result<(), E>){
+    println!("{:?}",return_result)
 }
