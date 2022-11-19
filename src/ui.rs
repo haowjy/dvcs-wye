8ee88fb2345cae8430f6c935561bb792e60ac155
cmbb <zhangyining098765432@126.com>
2022-11-19 16:13:56 -0500

    UI Module Version 1

--- src/ui.rs
+++ src/ui.rs
@@ -4,15 +4,51 @@ use std::path::Path;
 use crate::cmd_function;
 use crate::cmd_interface::{createonly, read_write};
 
-pub fn receive_input_command(input_test: &str) ->io::Result<()>{
-    Ok(())
-
+//type input=fn()->String;
+struct Command<T>{
+    path: T,
+    command_input:&'static str
+}
+struct UserInterface<T>{
+    commands: Vec<Command<T>>
 }
 
-fn match_command<'a>(wd:&'a str,input:Vec<&'a str>)->Vec<&'a str>{
-    input
+impl<T: Clone> UserInterface<T> {
+    fn new()-> Self{
+        Self{commands:vec![]}
+    }
+    pub fn receive_input_command(&mut self,input_test: T) ->io::Result<()>{
+        self.commands.push(Command{path: input_test.clone(),command_input: "input_test.clone()" });
+        Self::match_command(Command{path: input_test.clone(),command_input: "input_test.clone()" });
+        Ok(())
+    }
+
+    pub fn match_command(input:Command<T>)->String{
+        //input.path
+        let mut res:Result<&str,&str>=Err("1");
+        match input.command_input{
+            "add" => {res=crate::cmd_interface::read_write::add("input.path");}//1
+            "remove" => {res=crate::cmd_interface::read_write::remove("input.path");}//2
+            "commit" => {res=crate::cmd_interface::read_write::commit("input.path");}//3
+            "merge" => {res=crate::cmd_interface::read_write::merge("input.path");}//4
+            //"diff" => {res=crate::cmd_interface::read_write::diff(Some("input.path"),"input.path");}//5
+            "cat" => {res=crate::cmd_interface::read_write::cat(Some("input.path"),"input.path");}//6
+            "status" => {res=crate::cmd_interface::read_write::add("input.path");}//status
+            "log" => {res=crate::cmd_interface::read_write::commit("input.path");}//log
+            "heads" => {res=crate::cmd_interface::read_write::merge("input.path");}//heads
+            "clone" => {res=crate::cmd_interface::createonly::clone("input.path","input.path");}
+            _ => {}
+        }
+        Self::input_handling(res);
+        unimplemented!();
+    }
+
+    fn input_handling(return_result:Result<&str,&str>){
+        println!("{:?}",return_result)
+    }
+
+    fn input_handling_backup<E: std::fmt::Debug>(return_result:Result<(), E>){
+        println!("{:?}",return_result)
+    }
 }
 
-fn input_handling<E: std::fmt::Debug>(return_result:Result<(), E>){
-    println!("{:?}",return_result)
-}
