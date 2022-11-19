a3152f1b8c91563206c2b853511b36900dec7863
cmbb <zhangyining098765432@126.com>
2022-11-18 23:29:16 -0500

    UI Module Version 0

---
+++ src/ui.rs
@@ -0,0 +1,14 @@
+use std::io;
+
+pub fn receive_input_command(input_test: &str) ->io::Result<()>{
+    Ok(())
+
+}
+
+fn match_command(wd:path,input:Vec<&str>)->Vec<&str>{
+    input
+}
+
+fn input_handling(return_result:Result<(), E>){
+    println!(return_result)
+}
