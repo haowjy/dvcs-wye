756af76780be6fb5f75c82209c653436761bb398
YYWmus <88748057+YYWmus@users.noreply.github.com>
2022-11-27 18:06:13 -0500

    more funcs and error handling

--- src/vc/repository.rs
+++ src/vc/repository.rs
@@ -9,15 +9,154 @@ use sha2::{Sha256, Digest};
 use serde::{Serialize, Deserialize};
 
 use crate::dsr::*;
-
+use crate::vc::file::*;
+use crate::vc::revision::*;
 
 #[derive(Debug)] 
 pub struct Repo {
-    current_head: Option<String>, // sha_rev_id 
-    branch_heads: Option<HashMap<String,String>>, // <K=alias, V=sha_rev_id>
+    current_head: Option<String>, // alias 
+    branch_heads: HashMap<String,String>, // <K=alias, V=sha_rev_id>
     paths: RepoPaths,
-    revs: Option<DiGraphMap<String, String>>, // will change to wrapper struct
+    // revs: DiGraphMap<String, RevInfo>, // will change to wrapper struct
+    remote_head: Option<String>
 }
+// #[derive(Debug, Hash, PartialEq)]
+// struct RevInfo {
+//     id: String,
+//     parent_id: Option<String>,
+//     parent
+// }
+
+impl Repo {
+    pub fn get_current_head(&self) -> Option<Rev> {
+        match &self.current_head {
+            Some(alias) => Rev::from(self.branch_heads.get(alias)?),
+            None => None
+        }
+    }
+
+    pub fn get_rev(&self, rev_id: &str) -> Option<Rev> {
+        Rev::from(&path_compose(&self.paths.revs, rev_id))
+    }
+
+    pub fn commit(&mut self) -> Option<()> {
+        let mut stage = Rev::from(&self.paths.stage)?;
+        stage.store_files(&self.paths.files)?;// need to figure out logging to report
+        stage.update_time();
+        let commit_id = stage.gen_id()?;
+        stage.save()?;
+        if let Some(id) = self.branch_heads.get_mut(self.current_head.as_ref()?) {
+            *id = commit_id;
+        }
+        
+        Some(())
+    }
+
+    // pub fn repo_root()
+
+    pub fn get_log(&self) -> Option<Vec<&str>> {None} // *** to be implemented
+    // called by command "log", trace from the current head to parent(s) recursively to get a complete history of metadata in relevant revisions
+
+    // pub fn get_heads(&self) ->Option<Vec<&Rev>> {None}; // *** to be implemented
+    // get heads for all branches
+
+    pub fn new_head(&mut self, head_alias:&str, rev_id:&str) -> &Self {
+        self.branch_heads.entry(head_alias.to_string()).or_insert(rev_id.to_string());
+        self
+    } 
+
+    // pub fn fetch(&mut self, rwd:&str) -> &Self; // *** to be implemented
+
+// ------ newly added pub functions ------
+    pub fn get_file_content(&self, file_id: &str) -> Option<String> {
+        read_file_as_string(&path_compose(&self.paths.files, file_id)).ok()
+    }
+
+    pub fn get_stage(&self) -> Option<Rev> {
+        Rev::from(&self.paths.stage)
+    }
+
+
+// ------ private fns ------
+    fn save_stage(&self) -> Option<()> {
+        None // *** to be impl
+    }
+
+    fn save(&self) -> Option<()> { // Result<(), ()> {
+        // *** to add: save head
+        write_file(&self.paths.branch_heads, &serde_json::to_string(&self.branch_heads).ok()?).ok()
+    }
+
+}
+
+pub fn init() -> Option<()> { // Result<(),()>{ // error handling to be impl
+    let wd = get_wd_path();
+    let paths = RepoPaths::new(&wd);
+    // ***** error handling needed *****
+    // esp: handle running init again with existing repo
+    // try loading existing repo first 
+    // match let new_repo = load():
+    
+    create_dir(&paths.files).ok()?; // *** CHANGE ERR HANDLING LATER // root .dvcs automatically added
+    create_dir(&paths.revs).ok()?;
+    // create_file(&paths.branch_heads); 
+    // create_file(&paths.head);
+    let new_repo = Repo {
+        current_head: None, // Some("main".to_string())
+        branch_heads: HashMap::new(),
+        paths: paths,
+        remote_head: None,
+        // revs: None, // *** CHANGE LATER
+    };
+    new_repo.save();
+    return Some(());
+}
+
+pub fn load(wd:&str) -> Option<Repo> { // Result<Repo, ()>
+    let paths = RepoPaths::new(wd);
+    let load_repo = Repo {
+        current_head: read_file_as_string(&paths.head).ok(),
+        branch_heads: serde_json::from_str(&paths.branch_heads).ok()?,
+        paths: paths,
+        remote_head: None,// *** CHANGE LATER
+        // revs: None, 
+    };
+    return Some(load_repo);
+}
+
+// new pub fn
+pub fn clone(rwd:&str) -> Option<()> { // clone from a remote repo
+    None
+    // *** to be implemented
+    // needs to update remote head and have data structure tracking the rwd path,
+}
+
+pub fn add_file(abs_path: &str) -> Option<()> {
+    None // *** to be impl
+}
+
+pub fn remove_file(abs_path:&str) -> Option<()> {
+    None // *** to be impl
+}
+
+pub (super) fn sha<T: AsRef<[u8]> + ?Sized> (data: &T) -> String {
+    format!("{:x}", Sha256::digest(data))
+}
+
+// preliminary fn might change later or make a trait
+pub (super) fn sha_match<'a, T: Clone + Iterator + Iterator<Item=&'a String>> (sha: &'a String, pool: T) -> Vec<&'a String> {
+    let sha_len = sha.len();
+    pool.filter(|v| {
+        if v.len() < sha_len {
+            false
+        } else {
+            v[0..sha_len] == *sha
+        }
+    }).clone().collect::<Vec<_>>()
+}
+// resolving sha conflict
+// pub (crate) fn sha_resolve(sha: &str, pool: ) -> 
+
 
 #[derive(Debug)] 
 pub (super) struct RepoPaths { 
@@ -62,76 +201,6 @@ impl RepoPaths {
     }
 }
 
-impl Repo {
-
-    // fn commit(&self) -> Self {
-    //     let staged = Rev::from(self.paths.stage);
-        
-    //     // self.revs
-    // }
-
-    pub (crate) fn save(&self) -> () { // Result<(), ()> { 
-        () // *** CHANGE LATER
-
-        // write_result = write_file(&self.paths.head, &self.current_head.unwrap_or("").to_string());
-        // write_file(self.paths.branch_heads, serde::serialize(self.branch_heads)); // *** CHANGE LATER
-        // additional writing operations possible
-    }
-
-}
-
-pub fn init() -> Repo { // Result<(),()>{ // error handling to be impl
-    let wd = get_wd_path();
-    let paths = RepoPaths::new(&wd);
-    // ***** error handling needed *****
-    // esp: handle running init again with existing repo
-    // try loading existing repo first 
-    // match let new_repo = load():
-    
-    create_dir(&paths.files).ok(); // *** CHANGE ERR HANDLING LATER // root .dvcs automatically added
-    create_dir(&paths.revs).ok();
-    // create_file(&paths.branch_heads); 
-    // create_file(&paths.head);
-    let new_repo = Repo {
-        current_head: None,
-        branch_heads: None,
-        paths: paths,
-        revs: None, // *** CHANGE LATER
-    };
-    // new_repo.save();
-    return new_repo;
-}
-
-pub fn load(wd:&str) -> Repo { // Result<Repo, ()>
-    let paths = RepoPaths::new(wd);
-    let load_repo = Repo {
-        current_head: read_file_as_string(&paths.head).ok(),
-        branch_heads: None, // *** CHANGE, FOR TESTING ONLY
-
-        // branch_heads: read_file_as_string(&paths.branch_heads).ok(),
-        paths: paths,
-        revs: None, // *** CHANGE LATER
-    };
-    return load_repo;
-}
-
-pub (super) fn sha<T: AsRef<[u8]> + ?Sized> (data: &T) -> String {
-    format!("{:x}", Sha256::digest(data))
-}
-
-// preliminary fn might change later or make a trait
-pub (super) fn sha_match<'a, T: Clone + Iterator + Iterator<Item=&'a String>> (sha: &'a String, pool: T) -> Vec<&'a String> {
-    let sha_len = sha.len();
-    pool.filter(|v| {
-        if v.len() < sha_len {
-            false
-        } else {
-            v[0..sha_len] == *sha
-        }
-    }).clone().collect::<Vec<_>>()
-}
-// resolving sha conflict
-// pub (crate) fn sha_resolve(sha: &str, pool: ) -> 
 
 #[cfg(test)]
 mod tests {
