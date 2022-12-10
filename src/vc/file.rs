c3323663cc145f80bf94afddc4b6a4853b153c59
YYWmus <88748057+YYWmus@users.noreply.github.com>
2022-12-09 21:10:52 -0500

    added error handling, reconstructing ItemInfo struct and changed workflow

--- src/vc/file.rs
+++ src/vc/file.rs
@@ -1,36 +1,41 @@
 #[allow(dead_code)]
 #[allow(unused_imports)]
 use std::collections::HashMap;
-use std::time::SystemTime;
-use std::fs;
+// use std::time::SystemTime;
+// use std::fs;
 // external crates:
 
 use serde::{Serialize, Deserialize};
+use crate::ui::Errors;
 use crate::vc::repository::*;
 use crate::dsr::*;
 
-static STORAGE_DIR: &str =".dvcs/files"; //relative path hard coded, might change to use lazy_static macro 
+
+// static STORAGE_DIR: &str =".dvcs/files"; //relative path hard coded, might change to use lazy_static macro 
 
 // trait Info {
 //     loc_in_wd: String,
 //     content_id: String,
 // }
 
-#[derive(Debug, Clone, Serialize, Deserialize, Hash)] 
+#[derive(Debug, Clone, Serialize, Deserialize, Hash, PartialEq)] 
 pub struct ItemInfo {
-    name: Option<String>, // last component of path. Can be directory?
-    loc_in_wd: Option<String>, // wd RELATIVE path
-    content_id: Option<String>, // sha_id, only make an id when cached in repos (usually via commit)
-    entry_type: EntryType,
+    name: String, // last component of path. Can be directory?
+    loc_in_wd: String, // wd RELATIVE path
+    entry: EntryType
+
+    // content_id: Option<String>, // sha_id
+    // entry_type: EntryType,
     // metadata: Option<FileMetaData>,
 }
-#[derive(Debug, Clone, Serialize, Deserialize, Hash)] 
+#[derive(Debug, Clone, Serialize, Deserialize, Hash, PartialEq)] 
 pub enum EntryType {
-    Afile,
+    File(String),
     Dir, 
     Other 
 }
 
+use EntryType::{File, Dir, Other};
 
 // #[derive(Debug, Serialize, Deserialize)] 
 // struct FileMetaData {
@@ -50,52 +55,68 @@ pub enum EntryType {
 // }
 
 impl ItemInfo {
-    // MOVED TO Repo!
-    pub fn get_content(&self) -> Option<String> { // get cached content
-        let repo_storage_dir = get_repo_storage_dir()?;
-        match &self.content_id { 
-            Some(id) => read_file_as_string(&path_compose(&repo_storage_dir, &id)).ok(),
-            None => None
+    // CHANGE INCOMING: WILL MOVE TO Repo's get_file()
+    pub fn get_content(&self) -> Result<String, Errors> { // get cached content
+        
+        match self.entry {
+            File(id) => {
+                let repo_storage_dir = get_repo_storage_dir().ok_or(Errors::ErrUnknown)?;
+                read_file_as_string(&path_compose(&repo_storage_dir, &id))
+            },
+            _ => Err(Errors::ErrStr(format!("{}Not a File", self.name)))
         }
     }
 
-    pub fn get_file_wd_path(&self) -> Option<String> {
-        self.loc_in_wd.clone()
+    pub fn get_file_wd_path(&self) -> &str {
+        &self.loc_in_wd
     }
-
-    // pub fn get_cached_metadata(&self) -> Option<fs::Metadata> {
-    //     match &self.content_id { 
-    //         Some(id) => get_metadata(path_compose(STORAGE_DIR, id).as_str()).ok(),
-    //         None => None
-    //     }
-    // }
 
     // new pub fn, assisting make_wd
     // trial only, might move to repos
-    pub fn make_file(&self, wd:&str) -> Option<()> {
-        let wd_file_path = path_compose(wd, &self.loc_in_wd.as_ref()?);
+    pub fn make_file(&self, wd:&str) -> Result<(), Errors> {     // *** ERROR HANDLING
+        let wd_file_path = path_compose(wd, &self.loc_in_wd);
         let repo_storage_dir = path_compose(wd, ".dvcs/files");
-        let repo_storage_file = path_compose(&repo_storage_dir, self.content_id.as_ref()?);
-        copy_file(&repo_storage_file, &wd_file_path).ok()
-    }
-    pub (super) fn save_to_repo(&mut self) -> Option<()> {
-        let wd_root = get_wd_root()?;
-        let repo_storage_dir = path_compose(&wd_root, ".dvcs/files");
+        match self.entry {
+            File(id) => {
+                let repo_storage_file = path_compose(&repo_storage_dir, &id);
+                copy_file(&repo_storage_file, &wd_file_path)
+            },
+            Dir => {
+                if !is_path_valid(&wd_file_path) {
+                    create_dir(&wd_file_path)
+                } else {
+                    Ok(())
+                }
 
-        let content = read_file_as_string(&path_compose(&wd_root, self.loc_in_wd.as_ref()?)).ok()?;
-
-
-        let mut new_id = checked_sha(&content, &repo_storage_dir);
-        let storage_path = path_compose(&repo_storage_dir, &new_id);
-        if !is_path_valid(&storage_path) {
-            write_file(&storage_path, &content);
+            },
+            Other => Ok(()) // bypassing Other
         }
-        self.content_id = Some(new_id);
-        Some(())
     }
+
+
+    // // MOVING TO repository
+    // pub (super) fn save_to_repo(&mut self) -> Result<(), Errors>{ 
+    //     let wd_root = get_wd_root()?;
+    //     let repo_storage_dir = path_compose(&wd_root, ".dvcs/files");
+
+    //     copy_file(self.
+    //         path_compose(repo_storage_dir, &self.content_id))
+
+    //     let content = read_file_as_string(&path_compose(&wd_root, &self.loc_in_wd))?;
+
+
+    //     let mut new_id = checked_sha(&content, &repo_storage_dir);
+    //     let storage_path = path_compose(&repo_storage_dir, &new_id);
+
+    //     ()
+    //     if !is_path_valid(&storage_path) {
+    //         write_file(&storage_path, &content);
+    //     }
+    //     self.content_id = Some(new_id);
+    //     Ok(())
+    // }
 
 }
-
 // fn resolve_id_conflict(sha_id: &str, pool_path) -> Option<String> {
 //     let repo_storage_path = path_compose(pool_path, sha_id);
 //     if !is_path_valid(repo_storage_path) {
@@ -103,39 +124,46 @@ impl ItemInfo {
 //     }
 //     if content_    
 // } 
-pub (super) fn retrieve_info(abs_path: &str) -> Option<ItemInfo> {
-    if !is_path_valid(abs_path) {
-        println!("invalid path: {abs_path}"); // might replace with more organized error handling
-        return None;
-    }
-    let rel_path = get_rel_path(abs_path)?;
-    let meta = get_metadata(abs_path).ok()?; // *** ERROR HANDLING LATER
-    let mut t = EntryType::Other;
+pub fn retrieve_info(abs_path: &str) -> Result<ItemInfo, Errors> {
+    let rel_path = get_rel_path(abs_path).ok_or(Errors::ErrStr(format!("Cannot find the proper repository path for file {abs_path}")))?;
+
+    let meta = get_metadata(abs_path)?; 
+    let mut entry = EntryType::Other;
     if meta.is_dir() {
-        t = EntryType::Dir;
+        entry = EntryType::Dir;
     }
     else if meta.is_file() {
-        t = EntryType::Afile;
+        let id = get_id(abs_path)?;
+        entry = EntryType::File(id);
     }
-        
-    let info = ItemInfo {
-        name: get_name(&rel_path),
-        loc_in_wd: Some(rel_path),
-        content_id: None,
-        entry_type: t,
-    };
+    
+    Ok(ItemInfo {
+        name: get_name(&rel_path).ok_or(Errors::ErrStr(format!("Fail to get file name from path {abs_path}")))?,
+        loc_in_wd: rel_path,
+        entry: entry
+    })
+}
 
-    Some(info)
+
+pub fn get_id(file_path: &str) -> Result<String, Errors> {
+    let wd_root = get_wd_root()?;
+    let repo_storage_dir = path_compose(&wd_root, ".dvcs/files");
+
+    let content = read_file_as_string(file_path)?;
+
+    Ok(checked_sha(&content, &repo_storage_dir))
 }
 
 fn get_repo_storage_dir() -> Option<String> {
-    let wd_root = get_wd_root()?;
+    let wd_root = get_wd_root().ok()?;
     Some(path_compose(&wd_root, ".dvcs/files"))
 }
 
+
+
 #[cfg(test)]
 mod tests {
-    use super::*;
+    // use super::*;
 
     // #[test]
     // fn serialization() {
