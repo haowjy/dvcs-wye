eb9cb51a1b13e69d3652ac909637e356e847796b
YYWmus <88748057+YYWmus@users.noreply.github.com>
2022-12-09 21:09:53 -0500

    standardized error handling in revision pub fn, parent2, some reconstruction in process

--- src/vc/revision.rs
+++ src/vc/revision.rs
@@ -5,11 +5,12 @@ use std::collections::HashMap;
 use std::time::SystemTime;
 
 // external crates:
-use petgraph::graphmap::DiGraphMap;
-use sha2::{Sha256, Digest};
+// use petgraph::graphmap::DiGraphMap;
+// use sha2::{Sha256, Digest};
 use serde::{Serialize, Deserialize};
 
 use crate::dsr::*;
+use crate::ui::Errors;
 use crate::vc::repository::*;
 use crate::vc::file::*;
 
@@ -19,7 +20,8 @@ static REV_DIR: &str =".dvcs/revs"; //relative path hard coded, might change to 
 #[derive(Debug, Clone, Serialize, Deserialize)] // might impl Drop trait for save safety
 pub struct Rev {
     rev_id: Option<String>,
-    parent_id: Option<String>,
+    parent_id: Option<String>, 
+    parent_id2: Option<String>,
     user_id: Option<String>,
     time_stamp: SystemTime,
     manifest: HashMap<String, ItemInfo>,  // Hashmap<K: wd_relative_path, V: ItemInfo: file content id and metadata id
@@ -30,17 +32,20 @@ pub struct Rev {
 
 impl Rev {
     // *** deserialize not implemented, use serde_json for now
-    pub fn from(path: &str) -> Option<Rev> {
-        serde_json::from_str(&read_file_as_string(path).ok()?).ok()
+    pub fn from(path: &str) -> Result<Rev, Errors> {
+        match serde_json::from_str(&read_file_as_string(path)?){
+            Ok(rev) => Ok(rev),
+            Err(e) => Err(Errors::ErrSerde(e)),
+        }
         // rev.path_self = path.to_string();
         // Some(rev)
     }
 
     // *** serialize not implemented, use serde_json for now
-    pub (super) fn save(&self, rev_path: &str) -> Option<()> { //need to figure out destructor
+    pub (super) fn save(&self, rev_path: &str) -> Result<(), Errors> { //need to figure out destructor
         match &self.rev_id {
-            Some(id) => write_file(&path_compose(rev_path, &id), &serde_json::to_string(&self).ok()?).ok(),
-            None => None
+            Some(id) => write_file(&path_compose(rev_path, &id), &serialize(&self)?),
+            None => Err(Errors::ErrStr("Unable to save repo without id".to_string()))
         }
     }
 
@@ -48,6 +53,7 @@ impl Rev {
         Rev {
             rev_id: None,
             parent_id: None,
+            parent_id2:None,
             user_id: None,
             time_stamp: SystemTime::now(),
             manifest: HashMap::new(),
@@ -73,16 +79,24 @@ impl Rev {
         &self.manifest
     }
 
-    // NOTE: current vc doesn't track files moving from one subdirectory to another, 
-    pub (super) fn add_file(&mut self, abs_path: &str) -> Option<()> {
-        let rel_path = get_rel_path(abs_path)?;
-        if self.manifest.contains_key(&rel_path) {
-            return None;
-        }
+    pub fn get_log(&self) -> HashMap<String, String> {
+        let log = HashMap::new();
+        // self.rev_id = log()
+        log // *** TODO 
+    }
 
-        let new_entry:ItemInfo = retrieve_info(abs_path)?;
-        self.manifest.insert(rel_path, new_entry);
-        return Some(());
+    // NOTE: current vc doesn't track files moving from one subdirectory to another, 
+    pub (super) fn add_file(&mut self, abs_path: &str) -> Result<(), Errors> {
+        Err(Errors::ErrUnknown) // *** TO BE IMPL
+
+    //     let rel_path = get_rel_path(abs_path).ok_or(Errors::ErrUnknown)?; // change later
+    //     if self.manifest.contains_key(&rel_path) {
+    //         return None;
+    //     }
+
+    //     let new_entry:ItemInfo = retrieve_info(abs_path)?;
+    //     self.manifest.insert(rel_path, new_entry);
+    //     return Some(());
     }
 
     pub (super) fn remove_file(&mut self, abs_path: &str) -> Option<()> {None} // *** to be impl 
@@ -92,27 +106,33 @@ impl Rev {
         self
     }
 
-    pub fn store_files(&mut self, path: &str) -> Option<()> {// *** to be implemented
+    // *** To be Changed
+    pub (super) fn store_files(&mut self, path: &str) -> Result<(), Errors> {
         for (_, info) in self.manifest.iter_mut() {
-            info.save_to_repo()?;
+            ()
+            // info.save_to_repo()?;
+            
         }
     //         
-        Some(())
+        Ok(())
     }
 
-    pub (super) fn gen_id(&mut self) -> Option<String> {
+    pub (super) fn gen_id(&mut self) -> Result<String, Errors> {
         let wd = get_wd_root()?;
         let revs_dir = path_compose(&path_compose(&wd, ".dvcs"), "revs");
 
         self.parent_id = self.rev_id.clone(); // "inheritance"
         self.rev_id = None;
 
-        let id = checked_sha(&serde_json::to_string(&self.clone()).ok()?, &revs_dir);
+        let id = checked_sha(&serialize(&self.clone())?, &revs_dir);
         self.rev_id = Some(id.clone());
-        Some(id)
+        Ok(id)
     }
 }
 
+fn get_date_time(t: SystemTime) -> () {
+    () // TODO 
+}
 
 //     pub fn remove_file(&mut self, abs_path:&str) -> Self {
         // self.manifest
